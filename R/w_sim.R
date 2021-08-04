e1a7b26e648128b816b80913d5747186164c77b8
Logan Stundal <logan.stundal@gmail.com>
2021-08-03 19:10:41 -0500

    additional fix to w_sim

--- R/w_sim.R
+++ R/w_sim.R
@@ -15,13 +15,15 @@
 #' w_sim(20)
 
 
-w_sim <- function(n, cutoff = NULL){
+w_sim <- function(n, proportion_one = NULL){
   cords <- cbind(x = runif(n, -1, 1),
                  y = runif(n, -1, 1))
   w <- as.matrix(dist(cords))
 
-  if(is.null(cutoff)){
-    cutoff <- min(w[w>0])
+  if(is.null(proportion_one)){
+    cutoff <- quantile(w, probs = 0.1)
+  } else{
+    cutoff <- quantile(w, probs = proportion_one)
   }
 
   w[w <= cutoff] <- 1
@@ -31,3 +33,6 @@ w_sim <- function(n, cutoff = NULL){
 
   return(w)
 }
+
+# Note - does not exactly equal "proportion_one" due to zeros on main diagonal.
+# table(w_sim(100, proportion_one = 0.9))
